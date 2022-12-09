066ac4fb36a3e146c8d44da9fb5b37abefb2ea5d
Elion Rrahmani <elionnrrahmani@gmail.com>
2022-12-09 15:11:41 +0100

    Create

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtClub = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.playerview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.playerview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Club";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Description";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(91, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(138, 23);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtClub
+            // 
+            this.txtClub.Location = new System.Drawing.Point(91, 62);
+            this.txtClub.Name = "txtClub";
+            this.txtClub.Size = new System.Drawing.Size(138, 23);
+            this.txtClub.TabIndex = 1;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(91, 106);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(138, 23);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(319, 24);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(159, 23);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(12, 174);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(116, 174);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 2;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(224, 174);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 2;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(484, 24);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 2;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            // 
+            // playerview
+            // 
+            this.playerview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerview.Location = new System.Drawing.Point(319, 62);
+            this.playerview.Name = "playerview";
+            this.playerview.RowTemplate.Height = 25;
+            this.playerview.Size = new System.Drawing.Size(240, 150);
+            this.playerview.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playerview);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtClub);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.playerview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtName;
+        private TextBox txtClub;
+        private TextBox txtDescription;
+        private TextBox txtSearch;
+        private Button Save;
+        private Button Delete;
+        private Button Cancel;
+        private Button Search;
+        private DataGridView playerview;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string conString = @"Server=localhost;Database=playerdb;Uid=root;Pwd=1234;";
+        int playerId=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+              using (MySqlConnection mysqlcon = new MySqlConnection(conString))
+            {
+                mysqlcon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("PlayerAddOrEdit", mysqlcon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_Playerid", playerId);
+                mySqlCmd.Parameters.AddWithValue("_PlayerName", txtName.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Club", txtClub.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Desciption", txtDescription.Text.Trim());
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Succesfully saved!");
+            }  
+
+
+        }
+    }
+}
